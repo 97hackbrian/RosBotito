4f96d5e4a5e2f07db4cffc8ab5f416122526a2c9
97hackbrian <79449988.bd@gmail.com>
2024-07-18 19:49:48 -0400

    Full

--- src/navigation_tb3/launch/navigation.launch.py
+++ src/navigation_tb3/launch/navigation.launch.py
@@ -7,31 +7,25 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    config_dir = os.path.join(get_package_share_directory('navigation_tb3'),'config')
-    map_file = os.path.join(config_dir,'tb3_map.yaml')
-    param_file = os.path.join(config_dir,'tb3_nav2_params.yaml')
-    rviz_config_dir = os.path.join(config_dir,'navigation.rviz')
+    config_dir = os.path.join(get_package_share_directory('navigation_tb3'), 'config')
+    map_file = os.path.join(config_dir, 'mapCasa3.yaml')
+    param_file = os.path.join(config_dir, 'tb3_nav2_params.yaml')
+    rviz_config_dir = os.path.join(config_dir, 'navigation.rviz')
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory('turtlebot3_gazebo'),'/launch','/turtlebot3_world.launch.py'])
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'),'/launch','/bringup_launch.py']),
+            PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'), '/launch', '/bringup_launch.py']),
             launch_arguments={
-            'map':map_file,
-            'params_file': param_file}.items(),
-
+                'map': map_file,
+                'params_file': param_file
+            }.items(),
         ),
-
-
-    Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2_node',
-        arguments=['-d', rviz_config_dir],
-        output='screen'
-
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2_node',
+            arguments=['-d', rviz_config_dir],
+            output='screen'
         ),
     ])
+
